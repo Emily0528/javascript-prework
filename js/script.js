99a1a4f6368505e169728d4224f56ec75de1f9ba
Urszula Lukaszek <lu.urszula@gmail.com>
2026-02-09 07:41:45 +0100

    Add playerMove variable

---
+++ js/script.js
@@ -0,0 +1,13 @@
+var computerMove;
+var playerMove;
+
+computerMove = "kamień";
+playerMove = "papier";
+
+printMessage(
+  "Zagrałem " +
+    computerMove +
+    "! Jeśli Twój ruch to " +
+    playerMove +
+    ", to wygrywasz!",
+);
